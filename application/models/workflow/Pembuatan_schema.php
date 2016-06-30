6c2d833cad322cc46f9f0173bd5bcbd3e57b24d3
saepudin06 <saepudin06@gmail.com>
2016-06-30 16:12:13 +0700

    ''

---
+++ application/models/workflow/Pembuatan_schema.php
@@ -0,0 +1,133 @@
+<?php
+
+/**
+ * Document_type Model
+ *
+ */
+class Pembuatan_schema extends Abstract_model {
+
+    public $table           = "t_schema_example";
+    public $pkey            = "t_schema_example_id";
+    public $alias           = "scm";
+
+    public $fields          = array(
+                                't_schema_example_id'   => array('pkey' => true, 'type' => 'int', 'nullable' => true, 'unique' => true, 'display' => 'ID Doc type'),
+                                'schema_name'    => array('nullable' => false, 'type' => 'str', 'unique' => false, 'display' => 'Schema Name')
+                            );
+
+    public $selectClause    = "scm.*";
+    public $fromClause      = "t_schema_example scm";
+
+    public $refs            = array();
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    function validate() {
+        $ci =& get_instance();
+        $userinfo = $ci->ion_auth->user()->row();
+
+        if($this->actionType == 'CREATE') {
+            $p_rqst_type_id = 1;
+            $p_order_status_id = 1;
+            //do something
+            // example :
+            //$this->record['created_date'] = date('Y-m-d');
+            //$this->record['updated_date'] = date('Y-m-d');
+            $cust_order_id = $this->generate_id('T_CUSTOMER_ORDER');
+            $sql = "INSERT INTO T_CUSTOMER_ORDER (  T_CUSTOMER_ORDER_ID, 
+                                                    ORDER_NO, 
+                                                    P_RQST_TYPE_ID, 
+                                                    P_ORDER_STATUS_ID, 
+                                                    ORDER_DATE, 
+                                                    CREATION_DATE, 
+                                                    CREATED_BY, 
+                                                    UPDATED_DATE, 
+                                                    UPDATED_BY )
+                        VALUES (".$cust_order_id.",
+                                LPAD(T_CUSTOMER_ORDER_SEQ.NEXTVAL, 10, '0'),
+                                ".$p_rqst_type_id.",
+                                ".$p_order_status_id.",
+                                SYSDATE,
+                                SYSDATE,
+                                '".$userinfo->username."',
+                                SYSDATE,
+                                '".$userinfo->username."'
+                    )";
+
+            $dt = $this->db->query($sql);
+
+            $this->record[$this->pkey] = $this->generate_id($this->table);
+            $this->db->set('updated_date',"to_date('".date('Y-m-d')."','yyyy-mm-dd')",false);
+            $this->db->set('creation_date',"to_date('".date('Y-m-d')."','yyyy-mm-dd')",false);
+            $this->record['updated_by'] = $userinfo->username;
+            $this->record['created_by'] = $userinfo->username;
+            $this->record['t_customer_order_id'] = $cust_order_id;
+
+        }else{
+            //do something
+            //example:
+            //$this->record['updated_date'] = date('Y-m-d');
+            //if false please throw new Exception
+            $this->db->set('updated_date',"to_date('".date('Y-m-d')."','yyyy-mm-dd')",false);
+            $this->record['updated_by'] = $userinfo->username;
+        }
+        return true;
+    }
+
+    function removeChild($idd){
+        $sql = "select t_customer_order_id from t_schema_example where t_schema_example_id =".$idd;
+        $idd = $this->db->query($sql);
+        $rd = $idd->result_array();
+
+        try {
+            $this->db->where('T_CUSTOMER_ORDER_ID', $rd[0]['t_customer_order_id']);
+            $this->db->delete('T_CUSTOMER_ORDER');
+
+            return true;
+        } catch (Exception $e) {
+            return false;
+        }
+        
+    }
+
+    function submitWF($t_customer_order_id, $doc_type_id) {
+        $ci =& get_instance();
+        $userinfo = $ci->ion_auth->user()->row();
+        $username = $userinfo->username;
+
+        try {
+
+            $sql = "  BEGIN ".
+                            "  p_first_submit_engine(:i_doc_type_id, :i_cust_req_id, :i_username, :o_result_code, :o_result_msg ); END;";
+
+            $stmt = oci_parse($this->db->conn_id, $sql);
+
+            //  Bind the input parameter
+            oci_bind_by_name($stmt, ':i_doc_type_id', $doc_type_id);
+            oci_bind_by_name($stmt, ':i_cust_req_id', $t_customer_order_id);
+            oci_bind_by_name($stmt, ':i_username', $username);
+
+            // Bind the output parameter
+            oci_bind_by_name($stmt, ':o_result_code', $code, 2000000);
+            oci_bind_by_name($stmt, ':o_result_msg', $msg, 2000000);
+
+            ociexecute($stmt);
+
+            $data['success'] = true;
+            $data['error_code'] = $code;
+            $data['error_message'] = $msg;
+
+        } catch( Exception $e ) {
+            $data['success'] = false;
+            $data['message'] = $e->getMessage();
+        }
+
+        echo json_encode($data);
+        exit;
+    }
+    
+}
+
+/* End of file */
---
+++ application/views/workflow/pembuatan_schema.php
@@ -0,0 +1,299 @@
+<!-- breadcrumb -->
+<div class="page-bar">
+    <ul class="page-breadcrumb">
+        <li>
+            <a href="<?php base_url();?>">Home</a>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <a href="#">Workflow</a>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <span>Pembuatan Schema</span>
+        </li>
+    </ul>
+</div>
+<!-- end breadcrumb -->
+<div class="space-4"></div>
+<div class="row">
+    <div class="col-md-12">
+        <table id="grid-table"></table>
+        <div id="grid-pager"></div>
+    </div>
+</div>
+
+<script>
+    function submitWF(t_customer_order_id, schema_name) {        
+        result = confirm('Submit Nama Schema : ' + schema_name);
+        if (result) { 
+
+            $.ajax({
+                type: 'POST',
+                datatype: "json",
+                url: '<?php echo site_url('workflow_parameter/submitWF');?>',
+                data: { t_customer_order_id : t_customer_order_id, doc_type_id : 1 },
+                timeout: 10000,
+                success: function(data) {
+                    var response = JSON.parse(data);
+                    if(response.success) {
+                        jQuery('#grid-table').trigger("reloadGrid");
+                        swal("", "Submit Berhasil", "success");          
+                    }else {
+                        swal("", data.message, "warning");
+                    }
+                }
+            });
+        }
+        return false;
+    }
+
+    jQuery(function($) {
+        var grid_selector = "#grid-table";
+        var pager_selector = "#grid-pager";
+
+        jQuery("#grid-table").jqGrid({
+            url: '<?php echo WS_JQGRID."workflow.pembuatan_schema_controller/crud"; ?>',
+            datatype: "json",
+            mtype: "POST",
+            colModel: [
+                {label: 'ID',name: 't_schema_example_id', key: true, width: 35, sorttype: 'number', sortable: true, editable: true, hidden:true},
+                {
+                    label: 'Submit',
+                    name: 't_customer_order_id',
+                    width: 30, 
+                    align: "center",
+                    editable: false,
+                    formatter: function(cellvalue, options, rowObject) {
+                        var order = String(rowObject.schema_name);
+                        return '<button type="button" class="btn btn-white btn-sm btn-primary" onclick="submitWF('+cellvalue+',\''+order+'\');">Submit</button>';
+                    }
+                },
+                {label: 'Nama Schema',name: 'schema_name', width: 200, sortable: true, editable: true,
+                    editoptions: {
+                        size: 50,
+                        maxlength:64
+                    },
+                    editrules: {required: true}
+                }
+                
+            ],
+            height: '100%',
+            autowidth: true,
+            viewrecords: true,
+            rowNum: 10,
+            rowList: [10,20,50],
+            rownumbers: true, // show row numbers
+            rownumWidth: 35, // the width of the row numbers columns
+            altRows: true,
+            shrinkToFit: true,
+            multiboxonly: true,
+            onSelectRow: function (rowid) {
+                /*do something when selected*/
+
+            },
+            sortorder:'',
+            pager: '#grid-pager',
+            jsonReader: {
+                root: 'rows',
+                id: 'id',
+                repeatitems: false
+            },
+            loadComplete: function (response) {
+                if(response.success == false) {
+                    swal({title: 'Attention', text: response.message, html: true, type: "warning"});
+                }
+
+            },
+            //memanggil controller jqgrid yang ada di controller crud
+            editurl: '<?php echo WS_JQGRID."workflow.pembuatan_schema_controller/crud"; ?>',
+            caption: "Pembuatan Schema"
+
+        });
+
+        jQuery('#grid-table').jqGrid('navGrid', '#grid-pager',
+            {   //navbar options
+                edit: true,
+                editicon: 'fa fa-pencil blue bigger-120',
+                add: true,
+                addicon: 'fa fa-plus-circle purple bigger-120',
+                del: true,
+                delicon: 'fa fa-trash-o red bigger-120',
+                search: true,
+                searchicon: 'fa fa-search orange bigger-120',
+                refresh: true,
+                afterRefresh: function () {
+                    // some code here
+                    jQuery("#detailsPlaceholder").hide();
+                },
+
+                refreshicon: 'fa fa-refresh green bigger-120',
+                view: false,
+                viewicon: 'fa fa-search-plus grey bigger-120'
+            },
+
+            {
+                // options for the Edit Dialog
+                closeAfterEdit: true,
+                closeOnEscape:true,
+                recreateForm: true,
+                serializeEditData: serializeJSON,
+                width: 'auto',
+                errorTextFormat: function (data) {
+                    return 'Error: ' + data.responseText
+                },
+                beforeShowForm: function (e, form) {
+                    var form = $(e[0]);
+                    style_edit_form(form);
+
+                },
+                afterShowForm: function(form) {
+                    form.closest('.ui-jqdialog').center();
+                },
+                afterSubmit:function(response,postdata) {
+                    var response = jQuery.parseJSON(response.responseText);
+                    if(response.success == false) {
+                        return [false,response.message,response.responseText];
+                    }
+                    return [true,"",response.responseText];
+                }
+            },
+            {
+                //new record form
+                closeAfterAdd: false,
+                clearAfterAdd : true,
+                closeOnEscape:true,
+                recreateForm: true,
+                width: 'auto',
+                errorTextFormat: function (data) {
+                    return 'Error: ' + data.responseText
+                },
+                serializeEditData: serializeJSON,
+                viewPagerButtons: false,
+                beforeShowForm: function (e, form) {
+                    var form = $(e[0]);
+                    style_edit_form(form);
+                },
+                afterShowForm: function(form) {
+                    form.closest('.ui-jqdialog').center();
+                },
+                afterSubmit:function(response,postdata) {
+                    var response = jQuery.parseJSON(response.responseText);
+                    if(response.success == false) {
+                        return [false,response.message,response.responseText];
+                    }
+
+                    $(".tinfo").html('<div class="ui-state-success">' + response.message + '</div>');
+                    var tinfoel = $(".tinfo").show();
+                    tinfoel.delay(3000).fadeOut();
+
+                    return [true,"",response.responseText];
+                }
+            },
+            {
+                //delete record form
+                serializeDelData: serializeJSON,
+                recreateForm: true,
+                beforeShowForm: function (e) {
+                    var form = $(e[0]);
+                    style_delete_form(form);
+
+                },
+                afterShowForm: function(form) {
+                    form.closest('.ui-jqdialog').center();
+                },
+                onClick: function (e) {
+                    //alert(1);
+                },
+                afterSubmit:function(response,postdata) {
+                    var response = jQuery.parseJSON(response.responseText);
+                    if(response.success == false) {
+                        return [false,response.message,response.responseText];
+                    }
+                    return [true,"",response.responseText];
+                }
+            },
+            {
+                //search form
+                closeAfterSearch: false,
+                recreateForm: true,
+                afterShowSearch: function (e) {
+                    var form = $(e[0]);
+                    style_search_form(form);
+                    form.closest('.ui-jqdialog').center();
+                },
+                afterRedraw: function () {
+                    style_search_filters($(this));
+                }
+            },
+            {
+                //view record form
+                recreateForm: true,
+                beforeShowForm: function (e) {
+                    var form = $(e[0]);
+                }
+            }
+        );
+
+    });
+
+    function serializeJSON(postdata) {
+        var items;
+        if(postdata.oper != 'del') {
+            items = JSON.stringify(postdata, function(key,value){
+                if (typeof value === 'function') {
+                    return value();
+                } else {
+                  return value;
+                }
+            });
+        }else {
+            items = postdata.id;
+        }
+
+        var jsondata = {items:items, oper:postdata.oper, '<?php echo $this->security->get_csrf_token_name(); ?>' : '<?php echo $this->security->get_csrf_hash(); ?>'};
+        return jsondata;
+    }
+
+    function style_edit_form(form) {
+
+        //update buttons classes
+        var buttons = form.next().find('.EditButton .fm-button');
+        buttons.addClass('btn btn-sm').find('[class*="-icon"]').hide();//ui-icon, s-icon
+        buttons.eq(0).addClass('btn-primary');
+        buttons.eq(1).addClass('btn-danger');
+
+
+    }
+
+    function style_delete_form(form) {
+        var buttons = form.next().find('.EditButton .fm-button');
+        buttons.addClass('btn btn-sm btn-white btn-round').find('[class*="-icon"]').hide();//ui-icon, s-icon
+        buttons.eq(0).addClass('btn-danger');
+        buttons.eq(1).addClass('btn-default');
+    }
+
+    function style_search_filters(form) {
+        form.find('.delete-rule').val('X');
+        form.find('.add-rule').addClass('btn btn-xs btn-primary');
+        form.find('.add-group').addClass('btn btn-xs btn-success');
+        form.find('.delete-group').addClass('btn btn-xs btn-danger');
+    }
+
+    function style_search_form(form) {
+        var dialog = form.closest('.ui-jqdialog');
+        var buttons = dialog.find('.EditTable')
+        buttons.find('.EditButton a[id*="_reset"]').addClass('btn btn-sm btn-info').find('.ui-icon').attr('class', 'fa fa-retweet');
+        buttons.find('.EditButton a[id*="_query"]').addClass('btn btn-sm btn-inverse').find('.ui-icon').attr('class', 'fa fa-comment-o');
+        buttons.find('.EditButton a[id*="_search"]').addClass('btn btn-sm btn-success').find('.ui-icon').attr('class', 'fa fa-search');
+    }
+
+    function responsive_jqgrid(grid_selector, pager_selector) {
+
+        var parent_column = $(grid_selector).closest('[class*="col-"]');
+        $(grid_selector).jqGrid( 'setGridWidth', $(".page-content").width() );
+        $(pager_selector).jqGrid( 'setGridWidth', parent_column.width() );
+
+    }
+
+</script>
