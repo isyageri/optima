17ca2013b4d9f3be4884beb6080e5f641611e225
saepudin06 <saepudin06@gmail.com>
2016-07-15 10:25:42 +0700

    ''

---
+++ application/libraries/process/Log_billing_controller.php
@@ -0,0 +1,102 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Json library
+* @class Period_controller
+* @version 07/05/2015 12:18:00
+*/
+class Log_billing_controller {
+
+    function read() {
+
+        $page = getVarClean('page','int',1);
+        $limit = getVarClean('rows','int',5);
+        $sidx = getVarClean('sidx','str','job_control_id');
+        $sord = getVarClean('sord','str','desc');
+        $job_control_id = getVarClean('job_control_id','int', 0);
+
+        $data = array('rows' => array(), 'page' => 1, 'records' => 0, 'total' => 1, 'success' => false, 'message' => '');
+
+        try {
+
+            $ci = & get_instance();
+            $ci->load->model('process/log_billing');
+            $table = $ci->log_billing;
+
+            $req_param = array(
+                "sort_by" => $sidx,
+                "sord" => $sord,
+                "limit" => null,
+                "field" => null,
+                "where" => null,
+                "where_in" => null,
+                "where_not_in" => null,
+                "search" => $_REQUEST['_search'],
+                "search_field" => isset($_REQUEST['searchField']) ? $_REQUEST['searchField'] : null,
+                "search_operator" => isset($_REQUEST['searchOper']) ? $_REQUEST['searchOper'] : null,
+                "search_str" => isset($_REQUEST['searchString']) ? $_REQUEST['searchString'] : null
+            );
+
+            // Filter Table
+            $req_param['where'] = array('job_control_id = '.$job_control_id);
+
+            $table->setJQGridParam($req_param);
+            $count = $table->countAll();
+
+            if ($count > 0) $total_pages = ceil($count / $limit);
+            else $total_pages = 1;
+
+            if ($page > $total_pages) $page = $total_pages;
+            $start = $limit * $page - ($limit); // do not put $limit*($page - 1)
+
+            $req_param['limit'] = array(
+                'start' => $start,
+                'end' => $limit
+            );
+
+            $table->setJQGridParam($req_param);
+
+            if ($page == 0) $data['page'] = 1;
+            else $data['page'] = $page;
+
+            $data['total'] = $total_pages;
+            $data['records'] = $count;
+
+            $data['rows'] = $table->getAll();
+            $data['success'] = true;
+
+        }catch (Exception $e) {
+            $data['message'] = $e->getMessage();
+        }
+
+        return $data;
+    }
+
+    function crud() {
+
+        $data = array();
+        $oper = getVarClean('oper', 'str', '');
+        switch ($oper) {
+            case 'add' :
+
+            break;
+
+            case 'edit' :
+                
+            break;
+
+            case 'del' :
+               
+            break;
+
+            default :
+               // permission_check('view-batch-billing');
+                $data = $this->read();
+            break;
+        }
+
+        return $data;
+    }
+
+}
+
+/* End of file Period_controller.php */
---
+++ application/models/process/Log_billing.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Groups Model
+ *
+ */
+class Log_billing extends Abstract_model {
+
+    public $table           = "log_background_job";
+    public $pkey            = "job_control_id";
+    public $alias           = "log";
+
+    public $fields          = array();
+
+    public $selectClause    = "log.*";
+    public $fromClause      = "log_background_job log";
+
+    public $refs            = array();
+
+    function __construct() {
+        parent::__construct();
+        $this->db = $this->load->database('tosdb', TRUE);
+        $this->db->_escape_char = ' ';
+    }
+
+    function validate() {
+        $ci =& get_instance();
+
+        if($this->actionType == 'CREATE') {
+            //do something
+            // example :
+            //$this->record['created_date'] = date('Y-m-d');
+            //$this->record['updated_date'] = date('Y-m-d');
+
+            $this->record[$this->pkey] = $this->generate_id($this->table);
+        }else {
+            //do something
+            //example:
+            //$this->record['updated_date'] = date('Y-m-d');
+            //if false please throw new Exception
+        }
+        return true;
+    }
+
+}
+
+/* End of file Groups.php */
